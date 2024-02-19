2afd7357bcd792667077e39a9275fbc0aa5a9e5d
Oleksandra Nedashkivska <alexandranedashkovskaya10@gmail.com>
2024-02-20 01:46:36 +0200

    update searchbar component with new input

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,26 +1,34 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { TripsContext } from "../../store/TripsContext";
+import Input from "../UI/Input";
 
 import formatDate from "../../utils/formatDate";
+import searchIcon from "../../assets/search.png";
+import styles from "./searchbar.module.css";
 
 const SearchBar = () => {
   const { trips } = useContext(TripsContext);
 
+  const list = useMemo(
+    () =>
+      trips.map(({ id, city, startDate, endDate }) => ({
+        id,
+        value: `${city}: ${formatDate(startDate)} - ${formatDate(endDate)}`,
+      })),
+    [trips]
+  );
+
   return (
     <section>
-      <input
-        list="trip-options"
+      <img src={searchIcon} alt="Search icon" />
+      <Input
+        id="trip-options"
         name="trip-options"
+        type="datalist"
+        list={list}
         placeholder="Search your trip"
+        className={styles["search-bar"]}
       />
-      <datalist id="trip-options">
-        {trips.map(({ id, city, startDate, endDate }) => (
-          <option
-            key={id}
-            value={`${city}: ${formatDate(startDate)} - ${formatDate(endDate)}`}
-          ></option>
-        ))}
-      </datalist>
     </section>
   );
 };
